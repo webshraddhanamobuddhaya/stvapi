9dbc800abe06e5ebee34da9e7a2fb45d4df8d599
upathissa <amilaupathissa@gmail.com>
2018-09-03 17:56:19 +0530

    remove collection

--- app/Http/Controllers/Youtube/YoutubeControlle.php
+++ app/Http/Controllers/Youtube/YoutubeControlle.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Youtube;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Response;
+
 
 class YoutubeControlle extends Controller
 {
@@ -20,13 +22,19 @@ class YoutubeControlle extends Controller
 
         $url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=$chanelID&eventType=live&type=video&key=$apiKey";
 
-        $live_data = $client->get($url)->getBody()->getContents();
+        // $live_data = $client->get($url)->getBody()->getContents();
 
-        $arrayData = json_decode($live_data, true);
+        // $arrayData = json_decode($live_data, true);
 
-        $dataAll = collect($arrayData)->collapse();
+        // $dataAll = collect($arrayData)->collapse();
 
-        $liveStream_videoID = $dataAll->get('0')['id']['videoId'];
+        // $liveStream_videoID = $dataAll->get('0')['id']['videoId'];
+
+        $request = $client->get($url);
+        $response = $request->getBody();
+        $data = json_decode($response);
+        $liveStream_videoID = $data->items[0]->id->videoId;
+
 
         $youtubeID = [
             'video_id' => $liveStream_videoID,
@@ -36,6 +44,9 @@ class YoutubeControlle extends Controller
 
         return response($youtubeID, 200)
                   ->header('Content-Type', 'text/plain');
+
+
+        
 
     }
 }
