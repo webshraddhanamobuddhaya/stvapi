2d52bf64da9234fad26baad7147bc88e71042377
upathissa <amilaupathissa@gmail.com>
2018-10-09 13:57:38 +0530

    replace all models in Models folder form stv-new folder

--- app/Model/MetaData.php
+++ app/Model/MetaData.php
@@ -2,9 +2,15 @@
 
 namespace App\Model;
 
+use App\Model\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class MetaData extends Model
 {
     protected $table = 'wp_postmeta';
+
+    // Relationship with Post
+    public function post(){
+        return $this->belongsTo(Post::class,'post_id','ID');
+    }
 }
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,9 +2,25 @@
 
 namespace App\Model;
 
+use App\Model\MetaData;
+use App\Model\TermTaxonomy;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $table = 'wp_posts';
+
+    protected $primaryKey = 'ID';
+
+    // Relationship - One to Many with MataData
+    public function metadatas()
+    {
+        return $this->hasMany(MetaData::class,'post_id','ID');
+    }
+
+    public function termTaxonomys()
+    {
+        return $this->belongsToMany(TermTaxonomy::class,'wp_term_relationships','object_id','term_taxonomy_id');
+    }
+
 }
